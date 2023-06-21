c6e7cda2211d9c7c5ec4e9b62004affc9634e920
Шиленков вадим <vadimshilenkovlive@yandex.ru>
2023-06-21 18:45:19 +0300

    s3

--- main.java
+++ main.java
@@ -1,5 +1,7 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("dsvsv");
+        System.out.println("dsvsvsvzxv");
+        System.out.println("svsvzvz");
     }
 }
